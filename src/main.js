5c64d5743b3c85013258c05f4893ac065d33b7c8
JonasRossen <jona7906@stud.kea.dk>
2022-01-19 00:57:40 +0100

    final fix?

--- src/main.js
+++ src/main.js
@@ -3,61 +3,52 @@
 import "./scss/style.scss";
 import cart from "./cartObject.js";
 import "./toggle.js";
+import beers from "./beers.js";
+import handleMenu from "./handleMenu";
+
+let total = 0;
 
 window.addEventListener("DOMContentLoaded", () => {
-  // get();
+  calculateTotalSum();
+  start();
 });
 
-import beers from "./beers.js";
-
-const menuBtn = document.querySelector(".menu-btn");
-const hamburger = document.querySelector(".menu-btn__burger");
-const nav = document.querySelector(".nav");
-const menuNav = document.querySelector(".menu-nav");
-const navItems = document.querySelectorAll(".menu-nav__item");
-
-let showMenu = false;
-let total = 0;
-let localCart = JSON.parse(localStorage.getItem("cart"));
-//set total sum of beers, for each beer in beers we add the sum of each beer to the total sum.
-// the sum of each beer is calculated by multiplying its price by the amount selected
-beers.forEach((beer) => {
-  total += beer.price * cart[beer.name];
-});
-
-console.log(localCart);
-/* localCart.cart.forEach((beer) => {
-  total = beer.price;
-}); */
-/* total = localCart.total; */
-menuBtn.addEventListener("click", toggleMenu);
-
-document.querySelector(`.order_button`).addEventListener("click", () => {
-  document.querySelector(`#top_section`).scrollIntoView({ behavior: "smooth" });
-});
-
-function toggleMenu() {
-  if (!showMenu) {
-    hamburger.classList.add("open");
-    nav.classList.add("open");
-    menuNav.classList.add("open");
-    navItems.forEach((item) => item.classList.add("open"));
-
-    showMenu = true;
-  } else {
-    hamburger.classList.remove("open");
-    nav.classList.remove("open");
-    menuNav.classList.remove("open");
-    navItems.forEach((item) => item.classList.remove("open"));
-
-    showMenu = false;
-  }
+function calculateTotalSum() {
+  //set total sum of beers, for each beer in beers
+  // we add the sum of each beer to the total sum.
+  // the sum of each beer is calculated by multiplying its price by the amount selected
+  beers.forEach((beer) => {
+    total += beer.price * cart[beer.name];
+  });
 }
 
-start();
 function start() {
-  sorting();
   handleBeers(beers);
+  handleMenu();
+  addEventListeners();
+}
+
+function addEventListeners() {
+  sorting();
+  filtering();
+  document.querySelector(`.order_button`).addEventListener("click", () => {
+    document.querySelector(`#top_section`).scrollIntoView({ behavior: "smooth" });
+  });
+}
+
+function filtering() {
+  document.querySelectorAll(".filter_button").forEach((button) => {
+    button.addEventListener("mousedown", filterByType);
+  });
+}
+
+function filterByType(event) {
+  let filter = event.target.dataset.filterButton;
+  if (filter === "all") handleBeers(beers);
+  else {
+    let beerArr = beers.filter((beer) => beer.type === filter);
+    handleBeers(beerArr);
+  }
 }
 
 function sorting() {
@@ -71,68 +62,65 @@ function sortByPrice(event) {
     sortDir = "desc";
     event.target.dataset.sortDirection = "desc";
     beers.sort((a, b) => (a.price > b.price ? 1 : b.price > a.price ? -1 : 0));
-    handleBeers();
+    handleBeers(beers);
   } else {
     sortDir = "asc";
     event.target.dataset.sortDirection = "asc";
     beers.sort((a, b) => (a.price < b.price ? 1 : b.price < a.price ? -1 : 0));
-    handleBeers();
+    handleBeers(beers);
   }
-
-  console.log("price");
-
-  /* location.reload(); */
 }
+
 function sortByAlc(event) {
   let sortDir = event.target.dataset.sortDirection;
   if (sortDir === "asc") {
     sortDir = "desc";
     event.target.dataset.sortDirection = "desc";
     beers.sort((a, b) => (a.alc > b.alc ? 1 : b.alc > a.alc ? -1 : 0));
-    handleBeers();
+    handleBeers(beers);
   } else {
     sortDir = "asc";
     event.target.dataset.sortDirection = "asc";
     beers.sort((a, b) => (a.alc < b.alc ? 1 : b.alc < a.alc ? -1 : 0));
-    handleBeers();
+    handleBeers(beers);
   }
-  console.log("alcholol");
 }
 
 //create array of beers with properies
 //display each beer - "displayBeer(beer)"
-function handleBeers() {
-  /* location.reload(); */
+function handleBeers(beerArr) {
   document.querySelector(".beers_container ul").textContent = "";
-  beers.forEach((beer) => {
+  beerArr.forEach((beer) => {
     displayBeer(beer);
   });
 }
 
 function displayBeer(beer) {
   const template = document.querySelector("template");
-  if (!template) return null;
+  /*  if (!template) return null; */
+  const price = beer.price * cart[beer.name];
 
   const clone = template.content.cloneNode(true);
 
   const beerTotal = clone.querySelector(".beer-total");
   const beerName = clone.querySelector(".beer_name");
   const thePrice = clone.querySelector(".price");
-  const price = beer.price * cart[beer.name];
-  /* total += price; */
-  clone.querySelector(".price").textContent = price + "kr";
+  const quantity = clone.querySelector(".quantity");
   const totalSum = document.querySelector("#total_sum");
 
+  clone.querySelector(".price").textContent = price + "kr";
   clone.querySelector("img").src = beer.img;
-  beerName.textContent = beer.name;
   clone.querySelector(".beer_type").textContent = beer.type + " " + beer.alc + "%";
+
   thePrice.textContent = beer.price + "kr";
-  const quantity = clone.querySelector(".quantity");
-
+  beerName.textContent = beer.name;
   quantity.textContent = " " + "x" + cart[beer.name];
+  /* total += price; */
 
-  let beerTotalSum = cart[beer.name] * beer.price;
-  beerTotal.textContent = `${beerTotalSum}kr`;
+  //Adding sum for each beer by
+  //multiplying the number of beers selected with the price of the selected beer
+
+  beerTotal.textContent = `${calSumOfBeers(beer)}kr`;
 
   clone.querySelector("#remove").addEventListener("click", () => {
     if (cart[beer.name] === 0) {
@@ -144,11 +132,9 @@ function displayBeer(beer) {
       total -= beer.price;
       totalSum.innerHTML = `<h3>GO TO CART</h3> <br> <p>Total: ${total}kr</p>`;
       quantity.textContent = " " + "x" + cart[beer.name];
-      beerTotalSum = cart[beer.name] * beer.price;
-      beerTotal.textContent = `${beerTotalSum}kr`;
+      beerTotal.textContent = `${calSumOfBeers(beer)}kr`;
       localStorage.setItem("cart", JSON.stringify(cart));
     }
-    // cart[beer.name] === 0 ? (cart[beer.name] = 0) : (cart[beer.name] = cart[beer.name] - 1);
   });
 
   clone.querySelector("#add").addEventListener("click", () => {
@@ -157,8 +143,10 @@ function displayBeer(beer) {
     quantity.textContent = "x" + cart[beer.name];
 
     totalSum.innerHTML = `<h3>GO TO CART</h3> <br> <p>Total: ${total}kr</p>`;
-    beerTotalSum = cart[beer.name] * beer.price;
-    beerTotal.textContent = `${beerTotalSum}kr`;
+    /* beerTotalSum = cart[beer.name] * beer.price; */
+
+    beerTotal.textContent = `${calSumOfBeers(beer)}kr`;
+
     localStorage.setItem("cart", JSON.stringify(cart));
   });
   totalSum.innerHTML = `<h3>GO TO CART</h3> <br> <p>Total: ${total}kr</p>`;
@@ -166,16 +154,15 @@ function displayBeer(beer) {
   document.querySelector(".beers_container ul").appendChild(clone);
 }
 
-// function removeFromCart(event) {
-//   let item = event.target.getAttribute('data-name')
-//   console.log(cart)
-// }
+//Adding sum for each beer by
+//multiplying the number of beers selected with the price of the selected beer
+function calSumOfBeers(beer) {
+  let beerTotalSum = cart[beer.name] * beer.price;
 
-/* function myToggle() {
-  var element = document.body;
-  element.classList.add("light-mode");
-  element.classList.add("dark-mode");
+  return beerTotalSum;
+  /* 
+  beerTotalSum = cart[beer.name] * beer.price;
+  beerTotal.textContent = `${beerTotalSum}kr`; */
 }
- */
 document.body.style.display = "block";
 //https://stackoverflow.com/questions/4172281/force-browsers-to-load-css-before-showing-the-page
